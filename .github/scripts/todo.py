00bfe4f2219fe8f5a8a802cff505cf4098c824dd
Jordan <Jordan.Tsanga@hotmail.com>
2025-10-29 19:50:13 +1100

    Update todo.py

--- .github/scripts/todo.py
+++ .github/scripts/todo.py
@@ -1,1 +1,62 @@
+class Task: 
 
+  def __init__(self, titlem status+"ToDo"):
+      self.title = title
+      self.completed = False
+      self.status = status 
+
+  def mark_completed(self):
+      self.completed = True
+      self.status = "Done"
+
+  def __repr__(self):
+      return f"{self.title} - {'Done' if self.completed else 'ToDo'}"
+
+  def __str__(self):
+      return f"Task: {self.title}, Status: {self.status}"
+
+
+class TaskPool: 
+  def __init__(self):
+      self.tasks = []
+
+  def populate(self):
+    task1 = Task("Add Login UI", status="Done)
+    task2 = Task("Fix UI bug", status="Done)
+    task3 = Task("Write Tests", status="Done)
+    task4 = Task("Update login UI", status="Done)
+    task5 = Task("Update documentation", status="Done)
+    task6 = Task("Deploy to production", status="Done)
+
+    task1.mark_completed()
+    task2.mark_completed()
+    task3.mark_completed()
+
+    self.tasks = [task1, task2, task3, task4, task5, task6]
+
+  def add_task(self, task):
+      self.tasks.append(task)
+
+  def get_open_tasks(self):
+      return [task for task in self.tasks if task.status == "ToDo"]
+
+  def get_done_tasks(self):
+      return [task for task in self.tasks if task.status == "Done"]
+
+
+  def main():
+    pool = TaskPool()
+    pool.populate()
+
+    task_titles = [task.title for task in pool.get_open_tasks()]
+    print("ToDo Tasks:")
+    for title in task_titles 
+        print(title)
+
+    completed_titles = [task.title for task in pool.get_done_tasks()]
+    print("\nDone Tasks:")
+    for title in completed_titles:
+        print(title)
+
+if __name__ == "__main__":
+    main()
